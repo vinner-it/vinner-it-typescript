43be2bcea051e545824719c71f0716511f63a7e1
Giulio Caccin <giulio.caccin@scania.com>
2024-05-17 12:26:54 +0200

    use the interface luke

--- src/VinBuilder.ts
+++ src/VinBuilder.ts
@@ -4,24 +4,25 @@ export class VinBuilder {
     }
 }
 
-export class InvalidVin {
+export class InvalidVin implements Vin {
     vin: string;
+
     constructor(vin: string) {
         this.vin = vin;
     }
 }
 
-export class ValidVin {
+export class ValidVin implements Vin {
     vin: string;
+
     private constructor(vin: string) {
         this.vin = vin;
     }
 
     static createVin(vin: string): Vin {
-        if(/^[A-HJ-NPR-Z0-9]{17}$/.test(vin)) return new ValidVin(vin);
+        if (/^[A-HJ-NPR-Z0-9]{17}$/.test(vin)) return new ValidVin(vin);
         return new InvalidVin(vin);
     }
 
 }
-
-export type Vin = ValidVin | InvalidVin;
+export interface Vin {}
